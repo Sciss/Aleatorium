cba02f62046743256edaba5177a8da7c9b61fc1d
Hanns Holger Rutz <contact@sciss.de>
2021-06-19 11:45:52 +0200

    run-beta <>

--- run-beta.sh
+++ run-beta.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-echo "Aleatorium alpha"
+echo "Aleatorium beta"
 sleep 10
 qjackctl &
 sleep 10
 
-java -cp alpha/aleatorium-beta.jar de.sciss.aleatorium.Beta
+sudo java -cp beta/aleatorium-beta.jar:beta/lib/rpi-ws281x-java-2.0.0-SNAPSHOT.jar de.sciss.aleatorium.Light &
+
+sleep 4
+
+java -cp beta/aleatorium-beta.jar de.sciss.aleatorium.Beta
